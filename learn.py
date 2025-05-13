adc78f0131643a652cd017acdfd255515afe9e7f
passchaos <greedwolf.dss@gmail.com>
2025-05-13 21:27:04 +0800

    feat: add train and test logic

--- learn.py
+++ learn.py
@@ -1,43 +1,33 @@
 import torch
-from torch import backends, cuda, logit, mps, nn
-from torch.backends.mps import is_available
+
+from torch import nn
 from torch.utils.data import DataLoader
 from torchvision import datasets
 from torchvision.transforms import ToTensor
 
-training_data = datasets.FashionMNIST(
-    root = "data",
-    train = True,
-    download = True,
-    transform = ToTensor()
-)
+training_data = datasets.FashionMNIST(root="data", train=True, download=True, transform=ToTensor())
 
-test_data = datasets.FashionMNIST(
-    root = "data",
-    train = False,
-    download = True,
-    transform = ToTensor()
-)
+test_data = datasets.FashionMNIST(root="data", train=False, download=True, transform=ToTensor())
 
 batch_size = 64
-
 train_dataloader = DataLoader(training_data, batch_size=batch_size)
 test_dataloader = DataLoader(test_data, batch_size=batch_size)
 
-for x, y in test_dataloader:
-    print(f"Shape of x [N, C, H, W]: {x.shape}")
+for X, y in test_dataloader:
+    print(f"Shape of X [N, C, H, W]: {X.shape}")
     print(f"Shape of y: {y.shape} {y.dtype}")
     break
 
-device = ("cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu")
+device = torch.accelerator.current_accelerator().type if torch.accelerator.is_available() else "cpu"
 print(f"Using {device} device")
 
 class NeuralNetwork(nn.Module):
     def __init__(self):
         super().__init__()
+
         self.flatten = nn.Flatten()
         self.linear_relu_stack = nn.Sequential(
-            nn.Linear(28*28, 512),
+            nn.Linear(28 * 28, 512),
             nn.ReLU(),
             nn.Linear(512, 512),
             nn.ReLU(),
@@ -51,7 +41,77 @@ class NeuralNetwork(nn.Module):
 
 model = NeuralNetwork().to(device)
 print(model)
-print(model.parameters())
 
 loss_fn = nn.CrossEntropyLoss()
-optim = torch.optim.SGD(model.parameters(), lr=1e-3)
+optimizer = torch.optim.SGD(model.parameters(), lr = 1e-3)
+
+def train(dataloader, model: NeuralNetwork, loss_fn, optimizer):
+    size = len(dataloader.dataset)
+    print(f"dataset size: {size}")
+    model.train()
+
+    for batch, (X, y) in enumerate(dataloader):
+        X, y = X.to(device), y.to(device)
+        pred = model(X)
+        loss = loss_fn(pred, y)
+
+        loss.backward()
+        optimizer.step()
+        optimizer.zero_grad()
+
+        if batch % 100 == 0:
+            loss, current = loss.item(), (batch + 1) * len(X)
+            print(f"loss: {loss:>7f} [{current:>5d}/{size:>5d}]")
+        # print(f"batch: {batch}")
+        #
+def test(dataloader, model, loss_fn):
+    size = len(dataloader.dataset)
+    num_batches = len(dataloader)
+    model.eval()
+    test_loss, correct = 0, 0
+    with torch.no_grad():
+        for X, y in dataloader:
+            X, y = X.to(device), y.to(device)
+            pred = model(X)
+            test_loss += loss_fn(pred, y).item()
+            correct += (pred.argmax(1) == y).type(torch.float).sum().item()
+
+    test_loss /= num_batches
+    correct /= size
+    print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+
+# epochs = 5
+# for t in range(epochs):
+#     print(f"Epoch {t+1}\n--------------------")
+#     train(train_dataloader, model, loss_fn, optimizer)
+#     test(test_dataloader, model, loss_fn)
+
+# torch.save(model.state_dict(), "model.pth")
+# print("Saved PyTorch Model State to model.pth")
+print("Done!")
+
+model.load_state_dict(torch.load("model.pth", weights_only=True))
+
+classes = [
+    "T-shirt/top",
+    "Trouser",
+    "Pullover",
+    "Dress",
+    "Coat",
+    "Sandal",
+    "Shirt",
+    "Sneaker",
+    "Bag",
+    "Ankle boot"
+]
+
+model.eval()
+# print(f"test_data shape: {test_data.shape}")
+x, y = test_data[0][0], test_data[0][1]
+
+print(f"x: {x.shape} y: {y}")
+with torch.no_grad():
+    x = x.to(device)
+    pred = model(x)
+    predicted, actual = classes[pred[0].argmax(0)], classes[y]
+    print(f'Predicted: "{predicted}", Acutal: "{actual}"')
